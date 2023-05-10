45fe1909849679765f0a3957af84841f6acb136f
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-05-10 06:36:46 +0100

    our menu categories

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
-
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
   const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
     const newItems = items.filter((item) => item.catgory === category);
     return setMenuItems(newItems);
   };
@@ -17,7 +22,7 @@ function App() {
           <h2> our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>

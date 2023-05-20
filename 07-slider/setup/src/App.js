e99d96bcb8bd79ab209983aa7bfe6d944757b825
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-05-20 08:46:15 +0100

    slider working

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -5,6 +5,21 @@ import data from "./data";
 function App() {
   const [people, setpeople] = useState(data);
   const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
   return (
     <section className="section">
       <div className="title">
@@ -30,10 +45,10 @@ function App() {
                 <p className="title">{title}</p>
                 <p className="text">{quote}</p>
                 <FaQuoteRight className="icon" />
-                <button className="prev">
+                <button className="prev" onClick={() => setIndex(index - 1)}>
                   <FiChevronLeft />
                 </button>
-                <button className="next">
+                <button className="next" onClick={() => setIndex(index + 1)}>
                   <FiChevronRight />
                 </button>
               </div>

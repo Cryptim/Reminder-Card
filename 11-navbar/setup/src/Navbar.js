3d1897d4bde2373bc698c0dd2b2c7818225fce01
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-05-27 08:53:45 +0100

    navBars continues

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -13,8 +13,28 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container"></div>
-        <ul className="social-icons"></ul>
+        <div className="links-container show-container">
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );

75871a265599584131bd93f912919bc0c42c7b65
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-05-28 09:23:56 +0100

    click on showmodal

--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import { FaTimes } from "react-icons/fa";
 import { social, links } from "./data";
 
 const Sidebar = () => {
+  
   return (
     <aside className={`sidebar `}>
       <div className="sidebar-header">

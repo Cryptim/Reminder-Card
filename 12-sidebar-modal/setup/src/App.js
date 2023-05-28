2b0d5a8a8e4b8306ada0745aa8bb7552d18ae80f
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-05-28 08:19:14 +0100

    sidebar/modal-context

--- 12-sidebar-modal/setup/src/App.js
+++ 12-sidebar-modal/setup/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Modal from "./Modal";
 import Sidebar from "./Sidebar";
 import Home from "./Home";
+import { AppProvider } from "./context";
 function App() {
   return (
     <>

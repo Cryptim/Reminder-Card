9fae6cb620af2c6a6a06b2e92965d64002f032c0
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-06-04 03:47:09 +0100

    hosting random users on netlify

--- 17-random-person/setup/src/App.js
+++ 17-random-person/setup/src/App.js
@@ -1,72 +1,75 @@
 import React, { useState, useEffect } from "react";
-import {
-  FaEnvelopeOpen,
-  FaUser,
-  FaCalendarTimes,
-  FaMap,
-  FaPhone,
-  FaLock,
-} from "react-icons/fa";
+import { FaEnvelopeOpen, FaUser, FaMap, FaPhone, FaLock } from "react-icons/fa";
+
 const url = "https://randomuser.me/api/";
 const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [person, setPerson] = useState(null);
   const [title, setTitle] = useState("name");
   const [value, setValue] = useState("Random value");
+
   const handleValue = (e) => {
-    if (e.target.classList.contains(icons)) {
-      const newValue = e.target.dataset.data.label;
-      console.log(person);
-      console.log(newValue);
-      setTitle(newValue);
-      setValue(person[newValue]);
+    const newValue = e.target.dataset.label;
+    setTitle(newValue);
+    setValue(person[newValue]);
+  };
+
+  const getPerson = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const person = data.results[0];
+
+      if (person) {
+        const { phone, email } = person;
+        const { large: image } = person.picture;
+        const { login: { password } = {} } = person;
+        const { name: { first = "", last = "" } = {} } = person;
+        const { dob: { age } = {} } = person;
+        const { location: { street: { number, name } = {} } = {} } = person;
+
+        const newPerson = {
+          image,
+          phone,
+          email,
+          password,
+          age,
+          street: number && name ? `${number} ${name}` : null,
+          name: `${first}${last}`,
+        };
+
+        setPerson(newPerson);
+        setTitle("name");
+        setValue(newPerson.name);
+      }
+    } catch (error) {
+      console.error("Error fetching random user:", error);
+    } finally {
+      setLoading(false);
     }
   };
-  const getPerson = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    const person = data.results[0];
-    const { phone, email } = person;
-    const { large: image } = person.picture;
-    const {
-      login: { password },
-    } = person;
-    const { first, last } = person.name;
-    const {
-      dob: { age },
-    } = person;
-    const {
-      street: { number, name },
-    } = person.name;
-    const newPerson = {
-      image,
-      phone,
-      email,
-      password,
-      age,
-      street: `${number} ${name}`,
-      name: `${first}${last}`,
-    };
-    setPerson(newPerson);
-    setLoading(false);
-    setTitle("name");
-    setValue(newPerson.name);
-  };
+
+  useEffect(() => {
+    getPerson();
+  }, []);
+
   return (
     <main>
-      <div className="block bcg-black"></div>. block
+      <div className="block bcg-black"></div>
       <div className="block">
         <div className="container">
           <img
-            src={(person && person.image) || defaultImage}
+            src={person?.image || defaultImage}
             alt="random user"
             className="user-img"
           />
-          <p className="user-title">my {title} is </p>
+          <p className="user-title">My {title} is </p>
           <p className="user-value">{value}</p>
           <div className="values-list">
-            <FaUser />
             <button
               className="icon"
               data-label="name"
@@ -81,7 +84,6 @@ function App() {
             >
               <FaEnvelopeOpen />
             </button>
-            <FaUser />
             <button
               className="icon"
               data-label="street"
@@ -103,8 +105,8 @@ function App() {
             >
               <FaLock />
             </button>
-            <button className="btn" type="botton" onClick={getPerson}>
-              {loading ? "loading..." : "random user"}
+            <button className="btn" type="button" onClick={getPerson}>
+              {loading ? "Loading..." : "Random User"}
             </button>
           </div>
         </div>

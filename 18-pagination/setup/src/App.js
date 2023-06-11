5c96cbf4bcbc0fd858ceb7152bf638fc6e8a9bb5
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-06-11 02:46:49 +0100

    pagination complete

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -3,7 +3,34 @@ import { useFetch } from "./useFetch";
 import Follower from "./Follower";
 function App() {
   const { loading, data } = useFetch();
-  console.log(data);
+  // console.log(data);
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading, page]);
+  const handlepage = (index) => {
+    setPage(index);
+  };
+  const nextPage = (index) => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+  const prevPage = (index) => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
   return (
     <main>
       <div className="section-title">
@@ -12,10 +39,31 @@ function App() {
       </div>
       <section className="followers">
         <div className="container">
-          {data.map((follower) => {
+          {followers.map((follower) => {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
+        {!loading && (
+          <div className="btn-container">
+            <div className="prev-btn" onClick={prevPage}>
+              prev
+            </div>
+            {data.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`page-btn ${index === page ? "active-btn" : null}`}
+                  onClick={() => handlepage(index)}
+                >
+                  {index + 1}
+                </div>
+              );
+            })}
+            <div className="next-btn" onClick={nextPage}>
+              next
+            </div>
+          </div>
+        )}
       </section>
     </main>
   );
